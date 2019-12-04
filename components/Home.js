0841a60fff5fe58bd7ba1b8fd9db5b3c5d32fd17
unknown <alanmulana.id@gmail.com>
2019-12-04 14:41:11 +0700

    first commit

---
+++ components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+import { Container, Button, Text, CardItem } from 'native-base';
+
+export default class Home extends React.Component{
+    render() {
+        return(
+            <Container>
+                <Image/>
+
+            </Container>
+    )
+    }
+
+}
